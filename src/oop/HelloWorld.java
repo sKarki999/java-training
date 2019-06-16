250040e40f91968ea1e1e740f0e42c811efef8da
Sagun <sagun.karki27@gmail.com>
2019-06-16 18:46:21 +0200

    added new file helloworld.java

---
+++ src/oop/HelloWorld.java
@@ -0,0 +1,57 @@
+package oop;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Hello h = new Hello();
+		h.setName("Sagun");
+		h.setAddress("Fechenheim");
+		System.out.println(h.toString());
+		
+	}
+}
+
+	
+	 class Hello {
+		
+		String name;
+		String address;
+		
+		
+		// constructor
+		public Hello() {
+			
+			System.out.println("Welcome ");
+		}
+		
+		
+		// Getter and Setters
+		//Encapsulation
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		
+		// toString method		
+		@Override
+		public String toString() {
+			return "Hello [name=" + name + ", address=" + address + "]";
+		}
+		
+}
+
+	 
+	 
+	 
